34fe6966515fdb17bb16213d306dda00bf8cd8af
unknown <civbase@gmail.com>
2013-10-22 21:08:26 -0500

    Added more of zach's crap

--- www/script/roomselect.js
+++ www/script/roomselect.js
@@ -1,4 +1,4 @@
-$('#Submit').click(function() {
+$('#submit').click(function() {
 	if (validateDate(StartYear.value, StartMonth.value, StartDay.value) == -1) {
 		alert("Invalid Start Date");
 	}
@@ -7,8 +7,7 @@ $('#Submit').click(function() {
 		alert("Invalid End Date");
 	}
 	
-	$('#Submit').hide();
-	
+	$('#room_form').hide();
 	showMap(Building.value);
 	
 });
@@ -37,15 +36,21 @@ function validateDate(year, month, day) {
 }
 
 function showMap(building) {
+	var fileName;
 	if (building == "Carver") {
-	
+		fileName = "img/buildings/Carver.PNG"
 	} else if (building == "Gilman") {
-	
+		fileName = "img/buildings/Gilman.PNG"
 	} else if (building == "Hoover") {
-	
+		fileName = "img/buildings/Hoover.PNG"
 	} else if (building == "Howe") {
-	
+		fileName = "img/buildings/Howe.PNG"
 	} else if (building == "Memorial Union") {
-	
+		fileName = "img/buildings/MemorialUnion.PNG"
+	} else {
+		fileName = "img/buildings/none.png"
 	}
+	var str = '<img src="' + fileName + '">';
+	$('#map').append(building + " is located here: <br>");
+	$('#map').append(str);
 }
