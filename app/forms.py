c25ab65b0007200fa164b2b4de08f66c7c0b65fa
Branden Lange <branden.lange@webfilings.com>
2013-11-27 00:58:39 -0600

    Added more template forms

--- app/forms.py
+++ app/forms.py
@@ -3,11 +3,20 @@ from wtforms import TextField, PasswordField
 from wtforms.validators import Required
 
 class LoginForm(Form):
-	username = TextField('Username', validators = [Required()])
-	password = PasswordField('Password', validators = [Required()])
+	username = TextField('Username', validators=[Required()])
+	password = PasswordField('Password', validators=[Required()])
 
 class RegisterForm(Form):
-	email = TextField('Email', validators = [Required()])
-	username = TextField('Username', validators = [Required()])
-	password = PasswordField('Password', validators = [Required()])
-	confirmpassword = PasswordField('Confirm Password', validators = [Required()])
+	email = TextField('Email', validators=[Required()])
+	username = TextField('Username', validators=[Required()])
+	password = PasswordField('Password', validators=[Required()])
+	confirm_password = PasswordField('Confirm Password', validators=[Required()])
+
+class ForgotForm(Form):
+	email = TextField('Email', validators=[Required()])
+
+class SettingsForm(Form):
+	email = TextField('Email', validators=[])
+	username = TextField('Username', validators=[])
+	password = PasswordField('Password', validators=[])
+	old_password = PasswordField('Old Password', validators=[Required()])
