71d7a9bd9f6c9e707a08dc4c37d1225a510deea7
Branden Lange <branden.lange@webfilings.com>
2013-11-27 22:19:26 -0600

    Setup form validation

--- app/forms.py
+++ app/forms.py
@@ -1,22 +1,62 @@
 from flask.ext.wtf import Form
 from wtforms import TextField, PasswordField
-from wtforms.validators import Required
+from wtforms.validators import Required, AnyOf, NumberRange, EqualTo, Email
+
+alphanumeric = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_'
 
 class LoginForm(Form):
-	username = TextField('Username', validators=[Required()])
-	password = PasswordField('Password', validators=[Required()])
+	username = TextField(
+		'Username',
+		validators=[Required()])
+	password = PasswordField(
+		'Password',
+		validators=[Required()])
 
 class RegisterForm(Form):
-	email = TextField('Email', validators=[Required()])
-	username = TextField('Username', validators=[Required()])
-	password = PasswordField('Password', validators=[Required()])
-	confirm_password = PasswordField('Confirm Password', validators=[Required()])
+	email = TextField(
+		'Email',
+		validators=[Required(), 
+		            Email()])
+	username = TextField(
+		'Username',
+		validators=[Required(), 
+		            AnyOf(values=alphanumeric), 
+		            NumberRange(min=5, max=64)])
+	password = PasswordField(
+		'Password',
+		validators=[Required(),
+		            AnyOf(values=alphanumeric), 
+		            NumberRange(min=7, max=64), 
+		            EqualTo(fieldname='password')])
+	confirm_password = PasswordField(
+		'Confirm Password',
+		validators=[Required(), 
+		            AnyOf(values=alphanumeric), 
+		            NumberRange(min=7, max=64)])
 
 class ForgotForm(Form):
-	email = TextField('Email', validators=[Required()])
+	email = TextField(
+		'Email', 
+		validators=[Required()])
 
 class SettingsForm(Form):
-	email = TextField('Email', validators=[])
-	username = TextField('Username', validators=[])
-	password = PasswordField('Password', validators=[])
-	old_password = PasswordField('Old Password', validators=[Required()])
+	email = TextField(
+		'Email', 
+		validators=[])
+	username = TextField(
+		'Username', 
+		validators=[AnyOf(values=alphanumeric), 
+		            NumberRange(min=5, max=64)])
+	password = PasswordField(
+		'Password', 
+		validators=[AnyOf(values=alphanumeric), 
+		            NumberRange(min=7, max=64), 
+		            EqualTo(fieldname='password')])
+	confirm_password = PasswordField(
+		'Confirm Password',
+		validators=[Required(), 
+		            AnyOf(values=alphanumeric), 
+		            NumberRange(min=7, max=64)])
+	old_password = PasswordField(
+		'Old Password', 
+		validators=[Required()])
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, g, request
+from flask import render_template, redirect, url_for, g, request, flash, get_flashed_messages
 from app import app
 from forms import LoginForm, RegisterForm, ForgotForm, SettingsForm
 from models import User
@@ -6,26 +6,39 @@ from flask.ext.login import login_user, logout_user, current_user, login_require
 
 @app.route('/')
 @app.route('/index')
-def index():	
-	login_form = LoginForm()
-	register_form = RegisterForm()
-	return render_template('index.html', title="Login", login_form=login_form, register_form=register_form)
+def index():
+	return redirect(url_for('login'))
 
 @app.before_request
 def before_request():
 	g.user = current_user
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	return redirect(url_for('workspace'))
+	login_form = LoginForm(prefix="login")
+	register_form = RegisterForm(prefix="register")
+	if login_form.validate_on_submit():
+		print "boop"
+		return redirect(url_for('workspace'))
+	if register_form.validate_on_submit():
+		print "yoloswag"
+		return redirect(url_for('workspace'))
+	return render_template(
+		'index.html', 
+		message_box=get_message_box([login_form, register_form]),
+		title="Login", 
+		login_form=login_form, 
+		register_form=register_form)
 
 @app.route('/logout')
 def logout():
-	return redirect(url_for('index'))
+	return redirect(url_for('login'))
 
 @app.route('/workspace')
 def workspace():
-	return render_template('workspace.html', title="Workspace")
+	return render_template(
+		'workspace.html', 
+		title="Workspace")
 
 @app.route('/event')
 def event():
@@ -34,9 +47,36 @@ def event():
 @app.route('/forgot')
 def forgot():
 	forgot_form = ForgotForm()
-	return render_template('forgot.html', title="Forgot Password", forgot_form=forgot_form)
+	return render_template(
+		'forgot.html', 
+		title="Forgot Password", 
+		forgot_form=forgot_form)
 
 @app.route('/settings')
 def settings():
 	settings_form = SettingsForm()
-	return render_template('settings.html', title="Account Settings", settings_form=settings_form)
+	return render_template(
+		'settings.html', 
+		title="Account Settings", 
+		settings_form=settings_form)
+
+def get_message_box(forms):
+	flashed_messages = get_flashed_messages()
+	if flashed_messages:
+		return '<div class="box_spacer"></div>                                           \
+			<div class="message_box">                                                    \
+				<a href="javascript:;" class="cancel_message_button">x</a>               \
+				<span class="message">%s</span>                                          \
+			</div>' % flashed_message[0]
+	else:
+		for form in forms:
+			print form
+			for field in form.errors:
+				print form.errors[field]
+				if form.errors[field]:
+					return '<div class="box_spacer"></div>                               \
+						<div class="message_box error_box">                              \
+							<a href="javascript:;" class="cancel_message_button error_text">x</a> \
+							<span class="message error_text">%s</span>                   \
+						</div>' % form.errors[field][0]
+	return ""
