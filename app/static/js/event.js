7746cdddf2bc46d90957ef72f17f89f907dd25d5
Branden Lange <branden.lange@webfilings.com>
2013-11-30 14:54:17 -0600

    Started implementing the gridster on the events page template

--- app/static/js/event.js
+++ app/static/js/event.js
@@ -1,4 +1,6 @@
-$(".gridster ul").gridster({
+
+var gridster = $("#gridster ul").gridster({
         widget_margins: [10, 10],
-        widget_base_dimensions: [140, 140]
-    });
+        widget_base_dimensions: [150, 150],
+        min_cols: 6
+    }).data('gridster');
